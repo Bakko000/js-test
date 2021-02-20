199b7539c106bff5a4fe10bad3efa5b892d92d29
Bakko000 <corrado.baccheschi@gmail.com>
2021-02-20 10:45:36 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Import stylesheets
 import './style.css';
 
+
+// Funzione 
+function Clicca(){
+  alert("Hai cliccato il bottone");
+}
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<b>Ciao sono corrado</b>`;
